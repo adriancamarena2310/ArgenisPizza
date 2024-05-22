6e793dc7407420b75474d54ce8a4fd5de7ea7e0f
Adrian Camarena <adriancamarena2310@gmail.com>
2024-05-22 02:42:52 +0200

    feat: Asunto: Editar user

--- argenis_app/lib/src/screens/Profile_Screen.dart
+++ argenis_app/lib/src/screens/Profile_Screen.dart
@@ -1,17 +1,21 @@
+import 'package:argenis_app/src/models/user_model.dart';
+import 'package:argenis_app/src/screens/Home_Domicilio_Screen.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  final Usuario? user;
+  const ProfileScreen(this.user, {Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _ProfileScreenState createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  TextEditingController userNameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController numberController = TextEditingController();
+  late TextEditingController userNameController;
+  late TextEditingController passwordController;
+  late TextEditingController emailController;
+  late TextEditingController numberController;
 
   @override
   Widget build(BuildContext context) {
@@ -26,29 +30,83 @@ class _ProfileScreenState extends State<ProfileScreen> {
           children: [
             TextFormField(
               controller: userNameController,
-              decoration: InputDecoration(labelText: 'User Name'),
+              decoration: const InputDecoration(labelText: 'User Name'),
             ),
             TextFormField(
               controller: passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
+              decoration: const InputDecoration(labelText: 'Password'),
             ),
             TextFormField(
               controller: emailController,
-              decoration: InputDecoration(labelText: 'Email'),
+              decoration: const InputDecoration(labelText: 'Email'),
             ),
             TextFormField(
               controller: numberController,
-              decoration: InputDecoration(labelText: 'Number'),
+              decoration: const InputDecoration(labelText: 'Number'),
             ),
             ElevatedButton(
               onPressed: () {
-                //Pa guardar
+                _saveChanges();
               },
               child: const Text('Save'),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    userNameController = TextEditingController(text: widget.user?.userName);
+    passwordController = TextEditingController(text: widget.user?.password);
+    emailController = TextEditingController(text: widget.user?.email);
+    numberController = TextEditingController(text: widget.user?.number);
+  }
+
+  @override
+  void dispose() {
+    userNameController.dispose();
+    passwordController.dispose();
+    emailController.dispose();
+    numberController.dispose();
+    super.dispose();
+  }
+  
+  void _saveChanges() {
+    if(widget.user != null){
+      setState(() {
+        widget.user!.userName = userNameController.text;
+        widget.user!.password = passwordController.text;
+        widget.user!.email = emailController.text;
+        widget.user!.number = numberController.text;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Cambios guardados con éxito"))
+      );
+
+      // Realiza la animación de transición
+      Navigator.of(context).push(_createRoute());
+    }
+  }
+
+  Route _createRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => HomeDomicilioScreen(user: widget.user),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        final offsetAnimation = animation.drive(tween);
+
+        return SlideTransition(
+          position: offsetAnimation,
+          child: child,
+        );
+      },
     );
   }
 }
